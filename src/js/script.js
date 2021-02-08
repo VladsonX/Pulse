6bd357cf6bc4dc858ba6cf539c48d80d96c1b61e
VladsonX <vlad.novitskiy2002@mail.ru>
2021-02-08 18:32:56 +0300

    catalog
    catalog added

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.carousel__inner').slick({
-        speed: 1000,
+        speed: 700,
         slidesToShow: 1,
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/fourth_page/chevron-left-solid.svg"</button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/fourth_page/chevron-right-solid.svg"</button>',
